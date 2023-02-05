9a6e6e0cfe515bcac2be77af5ff2b2d3aa3d327a
Amit Kumar Thakur <kingston.amit9830@gmail.com>
2023-02-05 21:27:49 +0530

    wekly card completed

--- src/components/week/Weekly.jsx
+++ src/components/week/Weekly.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import { useSelector , useDispatch } from "react-redux";
+import styles from '../../styles/weekly.module.css' ; 
+import WeeklyCard from "./WeeklyCard";
 
 const Weekly = () => {
+  const habits = useSelector((state) => state.habitsReducer);
+  console.log(habits);
+  
   return (
-    <h1>weekly</h1>
-  )
-}
+    <div className={styles.container}>
+     { habits.list.map((habit , index)=>(
+          <WeeklyCard habit={habit} key={index}   />
+        ))
+     }
+    </div>
+  );
+};
 
-export default Weekly
+export default Weekly;
